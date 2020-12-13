6719fff868e2fac044cd8d0379afa78bd67fb487
yonseo <30378175+yonseo@users.noreply.github.com>
2020-12-13 09:28:01 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,37 +9,44 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <regex> // check for number or string
 
 using namespace std;
 
 //declare variables
-int result;
-int health;
-int experience;
-int gold;
+
+//monster
 int monster_health;
 int monster_attack;
-int user_input;
 
-
-
-int sum(int a, int b)
-{
-    result = a + b;
-    return (result);
-}
-
-int subtract(int a, int b)
-{
-    result = a - b;
-    return (result);
-}
+//player
+int result;
+int health = 100;
+int experience = 0;
+int attack = 10;
+int gold = 0;
+int playerInput;
 
 string room[3] = {
     "cave",
     "shop",
     "home"
 };
+
+bool isNumber(int playerInput){
+    
+    if (isdigit(playerInput) == false){
+        return false;
+    }
+    return true;
+
+}
+
+ 
+
+// number of elements in array
+int array_count_room = sizeof(room) / sizeof(room[0]);
+
 
 int main() {
 
@@ -50,15 +57,33 @@ int main() {
         cout << i << ") " << room[i] << endl;
     }
     
-    cin >> user_input;
-    if(user_input < 3)
+    cin >> playerInput;
+    if(isNumber(playerInput) == true)
     {
-        cout << "You have selected room: " << room[user_input] << endl;
+        //user entered a number
+        if(playerInput < array_count_room)
+        {
+            //within the array
+            cout << "You have selected room: " << room[playerInput] << endl;
+        }
+        else
+        {
+            //input is number
+            //outside of array
+            //no room found
+            cout << "no room found with that number" << endl;
+        }
     }
-    else
+    
+    if(isNumber(playerInput) == false)
     {
-        cout << "room was not found. Try again." << endl;
+        cout << "ERROR ENCOUNTERED!" << endl;
+        cout << "input is not a number" << endl;
+        //cout << "player input test: " <<  << endl;
+        //cout << "A room was not found. Try again." << endl;
     }
+    
+    
     
     return 0;
 }
